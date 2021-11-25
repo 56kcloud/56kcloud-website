84e9ff52b01ee99e5a0fdf50b372918e430a2b07
John Ingram <john.ingram@edeltech.ch>
2021-11-25 16:44:03 +0100

    [homepage] Add Next.js logo

--- components/homepage/logos.tsx
+++ components/homepage/logos.tsx
@@ -2,7 +2,7 @@ export default function Logos() {
   return (
     <div className="bg-gray-200">
       <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
           <div className="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
             <img
               className="h-16 sm:h-20"
@@ -26,6 +26,9 @@ export default function Logos() {
           </div>
           <div className="flex justify-center col-span-1 md:col-span-3 lg:col-span-1">
             <img className="h-16 sm:h-20" src="/images/logos/qt.svg" alt="Qt" />
+          </div>
+          <div className="flex justify-center col-span-1 md:col-span-3 lg:col-span-1">
+            <img className="h-16 sm:h-20" src="/images/logos/nextjs.svg" alt="Next.js" />
           </div>
         </div>
       </div>
