cc2611f58a6d0341ff0aa86ab32471b3970ede7f
JP Gehrig <jp.gehrig@edeltech.ch>
2021-11-28 20:45:08 +0100

    [dependencies] Update all

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import "../styles/global.css";
-import { AppProps } from "next/app";
+import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export const siteTitle = "Edeltech";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>{siteTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+            name="description"
+            content="We're a team of passionate engineers ready to develop your next web, mobile or desktop applications."
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
