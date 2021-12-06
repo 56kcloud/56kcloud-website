fbca79935c9981a6a759f8950a09058faad828ea
Jean-Pierre Gehrig <jp.gehrig@edeltech.ch>
2021-12-06 17:04:15 +0100

    Meet the team (#6)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import 'tailwindcss/tailwind.css'
-import Head from 'next/head'
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
 
 export const siteTitle = "Edeltech";
 
@@ -10,13 +10,13 @@ export default function App({ Component, pageProps }) {
         <title>{siteTitle}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta
-            name="description"
-            content="We're a team of passionate engineers ready to develop your next web, mobile or desktop applications."
+          name="description"
+          content="We're a team of passionate engineers ready to develop your next web, mobile or desktop applications."
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
