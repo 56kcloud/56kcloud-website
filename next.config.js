e6a64b91ca789ed6593d10c89fa342be9cc35351
JP Gehrig <jp.gehrig@edeltech.ch>
2021-12-07 19:37:07 +0100

    Redirect invalid blog post url from twitter

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   assetPrefix: isProd ? "https://www.edeltech.ch/" : "",
+  async redirects() {
+    return [
+      {
+        source: "/blog/2021-11-29-qt-for-mobile-overview/",
+        destination: "/blog/2021-11-29-qt-for-mobile-overview",
+        permanent: true,
+      },
+    ];
+  },
 };
