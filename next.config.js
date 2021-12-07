fad1bf4dba4fac8b443b34896e34580b24b6cc2a
JP Gehrig <jp.gehrig@edeltech.ch>
2021-12-07 19:44:58 +0100

    With `trailingSlash` set to `true`

--- next.config.js
+++ next.config.js
@@ -2,13 +2,5 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   assetPrefix: isProd ? "https://www.edeltech.ch/" : "",
-  async redirects() {
-    return [
-      {
-        source: "/blog/2021-11-29-qt-for-mobile-overview/",
-        destination: "/blog/2021-11-29-qt-for-mobile-overview",
-        permanent: true,
-      },
-    ];
-  },
+  trailingSlash: true,
 };
