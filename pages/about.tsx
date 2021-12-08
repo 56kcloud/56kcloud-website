a57a56ba1ce65c4c876aafaa58aca4632fb476d3
Jean-Pierre Gehrig <jp.gehrig@edeltech.ch>
2021-12-08 23:02:59 +0100

    Fix mobile nav (#13)

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Layout from "../components/layout";
-import ImageHeader from "../components/imageheader";
+import Nav from "../components/nav";
+import Hero from "../components/hero";
 import Team from "../components/team";
 import Contact from "../components/contact";
 
@@ -10,16 +10,19 @@ import { GetStaticProps } from "next";
 export default function About() {
   return (
     <>
-      <Head>
-        <title>Edeltech | About</title>
-      </Head>
-      <ImageHeader
-        title="About Edeltech"
-        tagline="We are located in Sion, the capital of the canton of Valais at
+      <header>
+        <Head>
+          <title>Edeltech | About</title>
+        </Head>
+        <Nav />
+        <Hero
+          title="About Edeltech"
+          tagline="We are located in Sion, the capital of the canton of Valais at
           the heart of the Swiss alps. Our office is at walking distance from the
           train station and the Valais campus of EPFL."
-        image="/images/sion.jpg"
-      />
+          image="/images/sion.jpg"
+        />
+      </header>
       <main>
         <Team people={team} />
         <Contact />
