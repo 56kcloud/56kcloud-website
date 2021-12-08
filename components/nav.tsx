2d0e0dc6c2582ba08e892152d39b1fdf45581e9e
JP Gehrig <jp.gehrig@edeltech.ch>
2021-12-08 23:01:08 +0100

    Prettier

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,47 +1,47 @@
 import Link from "next/link";
 import { useState } from "react";
 
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const navigation = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Blog', href: '/blog', current: false },
-  { name: 'About', href: '/about', current: false },
-]
+  { name: "Home", href: "/", current: true },
+  { name: "Blog", href: "/blog", current: false },
+  { name: "About", href: "/about", current: false },
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Nav() {
   return (
-    <Disclosure as="nav" className="bg-blue-900 z-10">
+    <Disclosure as="nav" className="z-10 bg-blue-900">
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto px-2 max-w-7xl sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-blue-200 hover:text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-blue-200 hover:text-white hover:bg-blue-700 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XIcon className="block w-6 h-6" aria-hidden="true" />
                   ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    <MenuIcon className="block w-6 h-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
                   <img
-                    className="block lg:hidden h-8 w-auto"
+                    className="block w-auto h-8 lg:hidden"
                     src="/images/edeltech-flower.svg"
                     alt="Workflow"
                   />
                   <img
-                    className="hidden lg:block h-8 w-auto"
+                    className="hidden w-auto h-8 lg:block"
                     src="/images/edeltech-flower.svg"
                     alt="Workflow"
                   />
@@ -53,10 +53,12 @@ export default function Nav() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-blue-900 text-white' : 'text-blue-300 hover:bg-blue-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-md font-medium uppercase'
+                          item.current
+                            ? "bg-blue-900 text-white"
+                            : "text-blue-300 hover:bg-blue-700 hover:text-white",
+                          "text-md px-3 py-2 font-medium rounded-md uppercase"
                         )}
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </a>
@@ -68,17 +70,19 @@ export default function Nav() {
           </div>
 
           <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
+            <div className="pb-3 pt-2 px-2 space-y-1">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-blue-900 text-white' : 'text-blue-300 hover:bg-blue-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
+                    item.current
+                      ? "bg-blue-900 text-white"
+                      : "text-blue-300 hover:bg-blue-700 hover:text-white",
+                    "block px-3 py-2 text-base font-medium rounded-md"
                   )}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
                 </Disclosure.Button>
@@ -88,5 +92,5 @@ export default function Nav() {
         </>
       )}
     </Disclosure>
-  )
+  );
 }
