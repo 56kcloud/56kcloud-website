c7c1d4512e450f3363f1c89fbffe6d8558ba95d3
YolanSavioz <yolan.savioz@edeltech.ch>
2021-12-10 10:49:04 +0100

    [components] map responsive

--- components/map.tsx
+++ components/map.tsx
@@ -15,8 +15,8 @@ export default function Map() {
                     </address>
                 </div>
             </div>
-            <div className="relative w-max mx-auto h-96 sm:h-96 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full mb-32 pl-2 ">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2760.293906417517!2d7.3494251160328545!3d46.2244996791173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478ec8341ef88b01%3A0xb49b036cb8062646!2sEdeltech!5e0!3m2!1sen!2sch!4v1639121306188!5m2!1sen!2sch" width="600" height="450" allowFullScreen={true} loading="lazy"></iframe>
+            <div className="relative mx-auto h-96 sm:h-96 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full mb-32 px-2 lg:pr-8">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2760.293906417517!2d7.3494251160328545!3d46.2244996791173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478ec8341ef88b01%3A0xb49b036cb8062646!2sEdeltech!5e0!3m2!1sen!2sch!4v1639121306188!5m2!1sen!2sch" width="max" height="max" allowFullScreen={true} loading="lazy" className="w-full h-full object-cover"></iframe>
             </div>
         </div>    
     );
