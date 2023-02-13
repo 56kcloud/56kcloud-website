0f77e5a97eba40d3e76dad3c6b701f53092794bd
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-02-13 16:52:47 +0100

    [app] Make intro responsive

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,9 +1,9 @@
 export default function Intro () {
   return (
-    <section className='py-24 px-36 bg-blue-dark'>
-      <div className='flex max-w-6xl mx-auto'>
-        <p className='text-white max-w-xs w-[45%] mr-auto text-base font-graphik font-light'>It's important to get the entire team on board and headed in the right direction on the same cloud journey. We align customers in their cloud-native story.</p>
-        <h2 className='text-white w-[55%] text-[calc(20px+1.6vw)] font-chap font-medium leading-tight'>56K.Cloud was founded to share our vision of the cloud with our partners and customers.</h2>
+    <section className='px-6 py-14 md:py-24 sm:px-14 lg:px-24 xl:px-36 bg-blue-dark'>
+      <div className='flex flex-col max-w-6xl mx-auto gap-y-8 md:flex-row'>
+        <p className='text-white w-full md:w-[42%] mr-auto text-base font-graphik font-light'>It's important to get the entire team on board and headed in the right direction on the same cloud journey. We align customers in their cloud-native story.</p>
+        <h2 className='text-white w-full md:w-[52%] text-[calc(20px+1.6vw)] font-chap font-medium leading-tight'>56K.Cloud was founded to share our vision of the cloud with our partners and customers.</h2>
       </div>
     </section>
   )
