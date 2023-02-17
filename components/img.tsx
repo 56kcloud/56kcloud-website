723d44274ae67b9361607bc616a17b8b48c8e45f
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-02-17 10:17:39 +0000

    wip

--- components/img.tsx
+++ components/img.tsx
@@ -1,15 +1,14 @@
-import { ImgPropsImpl } from '../models/Img.model'
+import { ImgProps, ImgPropsImpl } from '../models/Img.model'
 import Image from 'next/image'
 
 export default function Img (props: ImgPropsImpl) {
+  const imgProps = new ImgProps(props)
   return (
     <Image
       src={props.src}
       alt={props.alt}
       className='pointer-events-none'
-      fill={props.fill}
-      width={props.width}
-      height={props.height}
+      {...imgProps.specificHTMLProps}
     />
   )
 }
--- models/Img.model.tsx
+++ models/Img.model.tsx
@@ -9,9 +9,33 @@ type ImgFill = ImgBase & {
 }
 
 type ImgDimensionValues = ImgBase & {
-  fill: false
+  fill?: false
   width: number
   height: number
 }
 
 export type ImgPropsImpl = ImgDimensionValues | ImgFill
+
+export class ImgProps {
+  src: string
+  alt: string
+  className?: string
+  specificHTMLProps?: Record<string, string>
+
+  constructor (props: ImgPropsImpl) {
+    this.src = props.src
+    this.alt = props.alt
+    this.className = props.className
+    this.specificHTMLProps = this.specificHTMLPropsIntegration(props)
+  }
+
+  specificHTMLPropsIntegration (props: ImgPropsImpl) {
+    const HTMLProps = {}
+    const HTMLPropsKeys = Object.keys(props).filter(prop => !Object.keys(this).includes(prop))
+    HTMLPropsKeys.forEach(specificProp => {
+      const value = props[specificProp as keyof ImgPropsImpl]
+      Object.assign(HTMLProps, { [specificProp]: value })
+    })
+    return HTMLProps
+  }
+}
