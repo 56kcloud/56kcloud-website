e793085313f0d5705f111f44ad541b0f73a55806
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-02-28 09:25:27 +0100

    Fix divider light images

--- pages/training.tsx
+++ pages/training.tsx
@@ -32,7 +32,7 @@ export default function TrainingPage () {
         className='-mt-[10rem] sm:-mt-[15rem] md:-mt-[17rem] lg:-mt-[30rem] xl:-mt-[40rem] 2xl:-mt-[50rem]'
         width={1920} height={0} />
       <DetailsCardsWrapper cards={detailsCardsTraining} />
-      <BackgroundImage src='/images/division.png' alt='Plants divider' className='-mt-40 xl:-mt-64'
+      <BackgroundImage src='/images/divider-light.png' alt='Plants divider' className='-mt-40 xl:-mt-64'
         width={1920} height={0} />
       <Contact />
       <Footer version='illustration' />
