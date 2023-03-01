93520073724baf081ee43e0bba0cce7ba3bb3ce5
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-03-01 16:36:01 +0100

    [app] Translate partners

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -1,3 +1,4 @@
+import useTranslation from 'next-translate/useTranslation'
 import Head from 'next/head'
 import Layout from '../components/organisms/layout'
 import MediumTitleIntro from '../components/molecules/medium-title-intro'
@@ -9,16 +10,18 @@ import { lightIllustrationCardsPartners } from '../data/illustration-cards'
 import Footer from '../components/molecules/footer'
 
 export default function PartnersPage () {
+  const { t } = useTranslation('partners')
+
   return (
     <>
       <Layout>
         <Head>
-          <title>56K.Cloud | Partners</title>
+          <title>56K.Cloud | {t('tab')}</title>
         </Head>
-        <MediumTitleIntro title='On a Journey together'
-          textColLeft='We partner to enabled effective and successful projects together for customers.' />
-        <BaseCardsWrapper cards={baseCardsPartners} text='Key Partners' className='after:w-64' />
-        <BackgroundImage src='/images/divider-dark.png' alt='Plants divider' width={1920} height={0}
+        <MediumTitleIntro title={t('introTitle')}
+          textColLeft={t('introText')} />
+        <BaseCardsWrapper cards={baseCardsPartners} text={t('introSurtitle')} className='after:w-64' />
+        <BackgroundImage src='/images/divider-dark.png' alt={t('altDivider')} width={1920} height={0}
           className='-mt-[7rem] mb-16 md:mb-28 sm:-mt-36 md:-mt-44 lg:-mt-56 xl:-mt-72 2xl:-mt-96' />
         <IllustrationCardWrapper cards={lightIllustrationCardsPartners} theme='light' />
       </Layout>
