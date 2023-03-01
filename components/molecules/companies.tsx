c0abd915bdda68c561adc9833545866d2e4df3e8
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-03-01 13:50:37 +0100

    [app] Translate companies

--- components/molecules/companies.tsx
+++ components/molecules/companies.tsx
@@ -1,10 +1,13 @@
+import useTranslation from 'next-translate/useTranslation'
 import { companies } from '../../data/companies'
 import Icon from '../atoms/icon'
 
 export default function Companies () {
+  const { t } = useTranslation('companies')
+
   return (
     <section className='bg-blue-lighter section-padding' >
-      <h2 className='text-center title mb-14 xs-responsive-title'>These companies already trust us</h2>
+      <h2 className='text-center title mb-14 xs-responsive-title'>{t('title')}</h2>
       <div className='flex flex-wrap justify-center'>
         {companies.map((company) => (
           <li key={company.name} className='m-6 list-none'>
