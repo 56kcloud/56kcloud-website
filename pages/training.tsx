977b193c959ee9e3b6010a2f155fd6af1621f2d5
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-03-01 16:25:26 +0100

    [app] Translate training

--- pages/training.tsx
+++ pages/training.tsx
@@ -1,3 +1,4 @@
+import useTranslation from 'next-translate/useTranslation'
 import Head from 'next/head'
 import Layout from '../components/organisms/layout'
 import BackgroundImage from '../components/atoms/background-image'
@@ -10,29 +11,26 @@ import Contact from '../components/atoms/contact'
 import Footer from '../components/molecules/footer'
 
 export default function TrainingPage () {
+  const { t } = useTranslation('training')
+
   return (
     <Layout>
       <Head>
-        <title>56K.Cloud | Training</title>
+        <title>56K.Cloud | {t('tab')}</title>
       </Head>
       <LargeTitleIntro
-        surtitle='About our'
-        title='Training'
-        text='Our training concept is simple. Hands-on, real-world examples, customized to your specific environment
-        (Cloud, application, and business cases). 56K.Cloud works very closely with our partners to deliver the best
-        solutions. The 56K.Cloud team brings years of experience to the training presenting both the best practices
-        as well as how it works in production.' />
+        surtitle={t('introSurtitle')}
+        title={t('introTitle')}
+        text={t('introText')} />
       <Description
-        surtitle='What does our training include'
-        text='Not only do we provide training but we also offer pre-training calls and after-training support. We host
-        special training events in the Swiss Alps where we can take advantage of the great outdoors, while immersed in
-        the different training topics.'
+        surtitle={t('descriptionSurtitle')}
+        text={t('descriptionText')}
         details={detailsTraining} />
-      <BackgroundImage src='/images/mountain-background.png' alt='People standing in front of the mountain'
+      <BackgroundImage src='/images/mountain-background.png' alt={t('altMainIllustration')}
         className='-mt-[10rem] sm:-mt-[15rem] md:-mt-[17rem] lg:-mt-[30rem] xl:-mt-[40rem] 2xl:-mt-[50rem]'
         width={1920} height={0} />
       <DetailsCardsWrapper cards={detailsCardsTraining} />
-      <BackgroundImage src='/images/divider-light.png' alt='Plants divider' className='-mt-40 xl:-mt-64'
+      <BackgroundImage src='/images/divider-light.png' alt={t('altDivider')} className='-mt-40 xl:-mt-64'
         width={1920} height={0} />
       <Contact />
       <Footer version='illustration' />
