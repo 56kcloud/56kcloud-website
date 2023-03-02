82077b143c1ac91c0512acc4bb17a8787d1c35df
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-03-02 15:36:49 +0100

    [app] Translate illustration cards in partners page

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -1,4 +1,5 @@
 import useTranslation from 'next-translate/useTranslation'
+import { CardPropsImpl } from '../models/Card.model'
 import Head from 'next/head'
 import Layout from '../components/organisms/layout'
 import MediumTitleIntro from '../components/molecules/medium-title-intro'
@@ -6,11 +7,11 @@ import BaseCardsWrapper from '../components/organisms/base-cards-wrapper'
 import { baseCardsPartners } from '../data/base-cards'
 import BackgroundImage from '../components/atoms/background-image'
 import IllustrationCardWrapper from '../components/organisms/illustration-cards-wrapper'
-import { lightIllustrationCardsPartners } from '../data/illustration-cards'
 import Footer from '../components/molecules/footer'
 
 export default function PartnersPage () {
   const { t } = useTranslation('partners')
+  const illustrationCards: Array<CardPropsImpl> = t('partners:partners', { count: 3 }, { returnObjects: true })
 
   return (
     <>
@@ -23,7 +24,7 @@ export default function PartnersPage () {
         <BaseCardsWrapper cards={baseCardsPartners} text={t('cardsWrapperSurtitle')} className='after:w-64' />
         <BackgroundImage src='/images/divider-dark.png' alt={t('altDivider')} width={1920} height={0}
           className='-mt-[7rem] mb-16 md:mb-28 sm:-mt-36 md:-mt-44 lg:-mt-56 xl:-mt-72 2xl:-mt-96' />
-        <IllustrationCardWrapper cards={lightIllustrationCardsPartners} theme='light' />
+        <IllustrationCardWrapper cards={illustrationCards} theme='light' />
       </Layout>
       <footer>
         <Footer version='illustration' />
