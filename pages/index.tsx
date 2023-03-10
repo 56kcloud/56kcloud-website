10fb1a0b9862fd1fef16ac6e7db083f8c3673c26
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-03-10 13:17:07 +0100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Nav from '../components/molecules/nav'
 import Hero from '../components/molecules/hero'
 import SmallTitleIntro from '../components/molecules/small-title-intro'
 import IllustrationCardsWrapper from '../components/organisms/illustration-cards-wrapper'
-import { darkIllustrationCardsHome, lightIllustrationCardsHome } from '../data/illustration-cards'
+import { getDarkIllustrationCardsHome, lightIllustrationCardsHome } from '../data/illustration-cards'
 import BackgroundImage from '../components/atoms/background-image'
 import Companies from '../components/molecules/companies'
 import Footer from '../components/molecules/footer'
@@ -20,10 +20,10 @@ export default function HomePage () {
       <main>
         <SmallTitleIntro text={t('textIntro')}
           title={t('titleIntro')} />
-        <IllustrationCardsWrapper cards={darkIllustrationCardsHome} />
+        <IllustrationCardsWrapper cards={getDarkIllustrationCardsHome(t)} />
         <BackgroundImage src='/images/buro-background.png' alt={t('altImageBackground')} width={1920} height={0}
           className='bg-blue-lighter' />
-        <IllustrationCardsWrapper cards={lightIllustrationCardsHome} theme='light' />
+        {/* <IllustrationCardsWrapper cards={lightIllustrationCardsHome} theme='light' /> */}
         <Companies />
       </main>
       <Footer backgroundStyle='illustration' />
