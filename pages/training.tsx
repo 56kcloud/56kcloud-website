effb6d43221bf783c6fabe8493306fc06f68c8cb
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-03-11 19:10:12 +0100

    [app] Fix contact modal in training page

--- pages/training.tsx
+++ pages/training.tsx
@@ -15,24 +15,28 @@ export default function TrainingPage () {
 
   return (
     <Layout>
-      <Head>
-        <title>56K.Cloud | {t('tab')}</title>
-      </Head>
-      <LargeTitleIntro
-        surtitle={t('introSurtitle')}
-        title={t('introTitle')}
-        text={t('introText')} />
-      <Description
-        surtitle={t('descriptionSurtitle')}
-        text={t('descriptionText')}
-        details={details} />
-      <BackgroundImage src='/images/mountain-background.png' alt={t('altMainIllustration')}
-        className='-mt-[10rem] sm:-mt-[15rem] md:-mt-[17rem] lg:-mt-[30rem] xl:-mt-[40rem] 2xl:-mt-[50rem]'
-        width={1920} height={0} />
-      <DetailsCardsWrapper title='Our courses' cards={cardsDetails} styleTitle='text-white' />
-      <BackgroundImage src='/images/divider-light.png' alt={t('altDivider')} className='-mt-40 xl:-mt-64'
-        width={1920} height={0} />
-      <Contact toggleIsModalOpen={toggleIsOpen} />
+      {({ toggleIsOpen }) => (
+        <>
+          <Head>
+            <title>56K.Cloud | {t('tab')}</title>
+          </Head>
+          <LargeTitleIntro
+            surtitle={t('introSurtitle')}
+            title={t('introTitle')}
+            text={t('introText')} />
+          <Description
+            surtitle={t('descriptionSurtitle')}
+            text={t('descriptionText')}
+            details={details} />
+          <BackgroundImage src='/images/mountain-background.png' alt={t('altMainIllustration')}
+            className='-mt-[10rem] sm:-mt-[15rem] md:-mt-[17rem] lg:-mt-[30rem] xl:-mt-[40rem] 2xl:-mt-[50rem]'
+            width={1920} height={0} />
+          <DetailsCardsWrapper title='Our courses' cards={cardsDetails} styleTitle='text-white' />
+          <BackgroundImage src='/images/divider-light.png' alt={t('altDivider')} className='-mt-40 xl:-mt-64'
+            width={1920} height={0} />
+          <Contact toggleContactModal={toggleIsOpen} />
+        </>
+      )}
     </Layout>
   )
 }
