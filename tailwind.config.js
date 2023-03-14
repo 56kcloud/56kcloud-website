29b4f5ca0fd545081bc3130e51fc03f5bb7487f7
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-03-14 10:14:45 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './data/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
