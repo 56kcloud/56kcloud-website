078216eb7018578d79201f5643627ea98c3cd3f7
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-03-29 13:42:10 +0200

    wip

--- components/organisms/blog-cards-wrapper.tsx
+++ components/organisms/blog-cards-wrapper.tsx
@@ -1,12 +1,20 @@
-import BlogCard from '../../components/molecules/blog-card'
+import BlogCard, {BlogCardProps} from '../../components/molecules/blog-card'
 
-export default function BlogCardsWrapper({posts}) {
+export type BlogCardsWrapperProps = {
+  posts: Array<BlogCardProps>
+}
+
+export default function BlogCardsWrapper({posts}: BlogCardsWrapperProps) {
+  const postsSorted = posts.sort((a, b) => {
+    return a['properties'].published_at.date.start - b['properties'].published_at.date.start
+  })
+
   return (
     <section className='section-padding'>
       <div className='mx-auto max-w-7xl'>
         <div className='space-y-6 gap-x-6 sm:columns-2 lg:columns-3'>
-          {posts.map((post) => (
-            <div key={post.id}>
+          {postsSorted.map((post, idx) => (
+            <div key={idx}>
               <BlogCard post={post} />
             </div>
           ))}
