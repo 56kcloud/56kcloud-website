e70bcdb7a399bde52615115e1cf89f7ba7562bf6
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-03-29 14:19:43 +0200

    [app] Add z-index to badge component

--- components/atoms/badge.tsx
+++ components/atoms/badge.tsx
@@ -4,7 +4,7 @@ export type BadgeProps = {
 
 export default function Badge({children}: BadgeProps) {
   return (
-    <div className='absolute flex items-center justify-center px-4 py-1 bg-white rounded-lg left-6 top-6 w-fit \ 
+    <div className='absolute z-10 flex items-center justify-center px-4 py-1 bg-white rounded-lg left-6 top-6 w-fit \ 
       text-grey-dark'>
       <p className='text-sm font-medium'>{children}</p>
     </div>
