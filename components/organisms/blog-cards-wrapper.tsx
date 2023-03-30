48c22ed8f85aced2d74d131abd1d11417eecf585
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-03-30 16:23:33 +0100

    wip

--- components/organisms/blog-cards-wrapper.tsx
+++ components/organisms/blog-cards-wrapper.tsx
@@ -1,25 +1,31 @@
 import BlogCard, {BlogCardProps} from '../../components/molecules/blog-card'
+import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry'
 
 export type BlogCardsWrapperProps = {
   posts: Array<BlogCardProps>
 }
 
 export default function BlogCardsWrapper({posts}: BlogCardsWrapperProps) {
+
   const postsSorted = posts.sort((a, b) => {
     return new Date(b['properties'].published_at.date.start).valueOf() -
     new Date(a['properties'].published_at.date.start).valueOf()
   })
 
+  console.log(postsSorted)
+
   return (
     <section className='section-padding'>
       <div className='mx-auto max-w-7xl'>
-        <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
-          {postsSorted.map((post, idx) => (
-            <div key={idx}>
-              <BlogCard post={post} />
-            </div>
-          ))}
-        </div>
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{300: 1, 700: 2, 1000: 3}}
+        >
+          <Masonry gutter='1rem'>
+            {postsSorted.map((post, idx) => (
+              <BlogCard key={idx} post={post}/>
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
       </div>
     </section>
   )
