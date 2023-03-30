b6840ab202081898af6484dde5164d3f10de288b
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-03-30 15:50:45 +0100

    wip

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,23 +1,33 @@
 import {Client} from '@notionhq/client'
 import {authorsDbId, notionKey, postsDbId} from '../../config'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import {useRouter} from 'next/router'
 import BlogCardsWrapper from '../../components/organisms/blog-cards-wrapper'
 import Head from 'next/head'
 import Layout from '../../components/organisms/layout'
+import Link from 'next/link'
 import MediumTitleIntro from '../../components/molecules/medium-title-intro'
+import slugify from 'slugify'
 
 const notion = new Client({auth: notionKey})
 
 export default function Blog({posts}) {
-  const [listPosts, setListPosts] = useState(posts)
+  const [filteredPosts, setFilteredPosts] = useState(posts)
+  const router = useRouter()
   
-  function getFilteredPosts(posts) {
-    posts.filter((post) => {
-      post.properties.tags.multi_select[0].name === 'Company News'
-    })
+  function filterPosts(filterBy: string) {
+    setFilteredPosts(filterBy 
+      ? posts.filter((post) => {
+        return post.properties.tags.multi_select.map(select => slugify(select.name.toLowerCase())).includes(filterBy)
+      }) 
+      : posts)
   }
 
-  getFilteredPosts(posts)
+  useEffect(() => {
+    if (router.isReady) {
+      filterPosts(router.query.tag?.toString() || '')
+    }
+  }, [router])
 
   return (
     <Layout>
@@ -25,13 +35,18 @@ export default function Blog({posts}) {
         <title>56K.Cloud | Blog</title>
       </Head>
       <MediumTitleIntro title='From the blog' />
-      <BlogCardsWrapper posts={posts} />
+      <Link href={'/blog?tag=aws'}>AWS</Link>
+      <Link href={'/blog?tag=company-news'}>company news</Link>
+      <Link href={'/blog?tag=5g'}>5G</Link>
+      <BlogCardsWrapper posts={filteredPosts} />
     </Layout>
   )
 }
 
 export async function getStaticProps() {
   const authors = (await notion.databases.query({database_id: authorsDbId})).results || []
+  const db = await notion.databases.retrieve({database_id: postsDbId})
+  console.log(db.properties.tags.multi_select.options)
   const postsQuery = (await notion.databases.query({database_id: postsDbId,
     sorts: [
       {
