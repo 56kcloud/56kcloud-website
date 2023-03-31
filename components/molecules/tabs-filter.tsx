89905443070d4a36dc1ce40123bfc31fdbb4f0d3
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-03-31 15:56:42 +0200

    [page/blog] Create tabs filter molecule

---
+++ components/molecules/tabs-filter.tsx
@@ -0,0 +1,63 @@
+import {ArrowLeftIcon, ArrowRightIcon} from '@heroicons/react/outline'
+import {useRef} from 'react'
+import Link from 'next/link'
+
+const tabs = [
+  {href: '/blog', name: 'All'},
+  {href: '/blog?tag=aws', name: 'AWS'},
+  {href: '/blog?tag=company-news', name: 'Company News'},
+  {href: '/blog?tag=5g', name: '5G'},
+  {href: '/blog?tag=how-to', name: 'How To'},
+  {href: '/blog?tag=iot', name: 'IoT'},
+  {href: '/blog?tag=cloud', name: 'Cloud'},
+  {href: '/blog?tag=docker', name: 'Docker'},
+  {href: '/blog?tag=arm', name: 'ARM'},
+  {href: '/blog?tag=gitlab', name: 'GitLab'},
+  {href: '/blog?tag=aws-graviton', name: 'AWS Graviton'},
+  {href: '/blog?tag=devops', name: 'DevOps'},
+  {href: '/blog?tag=agile', name: 'Agile'},
+  {href: '/blog?tag=digital-transformation', name: 'Digital Transformation'},
+  {href: '/blog?tag=devsecops', name: 'DevSecOps'},
+  {href: '/blog?tag=gruntworks', name: 'Gruntworks'},
+  {href: '/blog?tag=github', name: 'GitHub'},
+  {href: '/blog?tag=terraform', name: 'Terraform'},
+  {href: '/blog?tag=edge', name: 'Edge'},
+  {href: '/blog?tag=monitoring', name: 'Monitoring'},
+  {href: '/blog?tag=community', name: 'Community'},
+  {href: '/blog?tag=cdk', name: 'CDK'},
+  {href: '/blog?tag=iac', name: 'IaC'},
+  {href: '/blog?tag=azure', name: 'Azure'},
+  {href: '/blog?tag=partners', name: 'Partners'}
+]
+
+export default function TabsFilter() {
+  const tabsListRef = useRef<HTMLUListElement>(null)
+  
+  function scrollLeft() {
+    tabsListRef.current.scrollTo({left: - 200, behavior: 'smooth'})
+  }
+
+  function scrollRight() {
+    tabsListRef.current.scrollTo({left: + 200, behavior: 'smooth'})
+  }
+  
+  return (
+    <div className='relative mb-8 max-w-7xl'> {/* Maybe remove max-w-7xl className */}
+      <div className='absolute top-0 left-0 flex items-center justify-center w-10 h-full bg-gradient-to-r \
+       from-blue-lighter to-transparent'>
+        <ArrowLeftIcon onClick={scrollLeft} className='w-6 h-6 cursor-pointer' />
+      </div>
+      <ul ref={tabsListRef} className='flex overflow-x-hidden gap-x-3'>
+        {tabs.map((tab, idx) => (
+          <li key={idx} className='px-5 py-3 bg-white rounded-xl whitespace-nowrap'>
+            <Link href={tab.href}>{tab.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <div className='absolute top-0 right-0 flex items-center justify-center w-10 h-full bg-gradient-to-l \
+       from-blue-lighter to-transparent'>
+        <ArrowRightIcon onClick={scrollRight} className='w-6 h-6 cursor-pointer' />
+      </div>
+    </div>
+  )
+}
--- components/organisms/blog-cards-wrapper.tsx
+++ components/organisms/blog-cards-wrapper.tsx
@@ -1,6 +1,6 @@
 import BlogCard, {BlogCardProps} from '../../components/molecules/blog-card'
-import Filter from '../molecules/filter'
 import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry'
+import TabsFilter from '../molecules/tabs-filter'
 
 export type BlogCardsWrapperProps = {
   posts: Array<BlogCardProps>
@@ -10,7 +10,7 @@ export default function BlogCardsWrapper({posts}: BlogCardsWrapperProps) {
   return (
     <section className='section-padding'>
       <div className='mx-auto max-w-7xl'>
-        <Filter />
+        <TabsFilter />
         <ResponsiveMasonry
           columnsCountBreakPoints={{300: 1, 700: 2, 1000: 3}}
         >
