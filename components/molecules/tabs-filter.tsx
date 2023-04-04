1e1d5fcbcc678a2c1a6cb5380f3cc1f21693acf3
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-04-04 13:42:59 +0200

    [page/blog] Add tabs slider style

--- components/molecules/tabs-filter.tsx
+++ components/molecules/tabs-filter.tsx
@@ -1,6 +1,8 @@
 import 'pure-react-carousel/dist/react-carousel.es.css'
 import {ButtonBack, ButtonNext, CarouselProvider, Slider} from 'pure-react-carousel'
+import {useRouter} from 'next/router'
 import Link from 'next/link'
+import classNames from '../../utils/classes'
 
 const tabs = [
   {href: '/blog', name: 'All'},
@@ -31,20 +33,24 @@ const tabs = [
 ]
 
 export default function TabsFilter() {
+  const router = useRouter()
+  
   return (
     <CarouselProvider className='mb-10 overflow-hidden' naturalSlideWidth={100} naturalSlideHeight={125} 
       totalSlides={25}>
       <div className='relative flex items-center px-8'>
         <ButtonBack role='button' aria-label='slide backward' className='absolute left-0 z-50'>
           <svg width={8} height={14} viewBox='0 0 8 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <path d='M7 1L1 7L7 13' stroke='#1f2c73' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' />
+            <path d='M7 1L1 7L7 13' stroke='#636e8e' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' />
           </svg>
         </ButtonBack>
         <div className='absolute z-30 w-10 h-12 left-6 bg-gradient-to-r from-blue-lighter'></div>
         <Slider>
           <ul className='flex overflow-x-hidden gap-x-3'>
             {tabs.map((tab, idx) => (
-              <Link key={idx} href={tab.href} className='px-5 py-3 bg-white rounded-xl whitespace-nowrap'>
+              <Link key={idx} href={tab.href} className={classNames(router.asPath === tab.href 
+                ? 'text-grey-dark bg-slate-200' : 'text-slate-400 hover:text-grey-dark', 
+              'text-sm 2xl:text-base font-normal relative px-5 py-3 bg-white rounded-xl whitespace-nowrap')}>
                 {tab.name}
               </Link>
             ))}
@@ -53,7 +59,7 @@ export default function TabsFilter() {
         <div className='absolute z-30 w-10 h-12 right-6 bg-gradient-to-l from-blue-lighter'></div>
         <ButtonNext role='button' aria-label='slide forward' className='absolute right-0 z-50'>
           <svg width={8} height={14} viewBox='0 0 8 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <path d='M1 1L7 7L1 13' stroke='#1f2c73' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' />
+            <path d='M1 1L7 7L1 13' stroke='#636e8e' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' />
           </svg>
         </ButtonNext>
       </div>
