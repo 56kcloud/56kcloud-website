c6a570fd8bc45430c0bedc3f8b55f9292a06d1a8
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-04-05 17:04:45 +0200

    [page/blog] Make tabs slider responsive

--- components/molecules/tabs-filter.tsx
+++ components/molecules/tabs-filter.tsx
@@ -24,12 +24,12 @@ export default function TabsFilter({tags}: TabsFilterProps) {
   
   return (
     <div className='relative mb-10'>
-      <div className='absolute top-0 z-50 w-10 h-full left-14 bg-gradient-to-r from-blue-lighter' />
+      <div className='absolute top-0 z-30 w-10 h-full left-10 2xl:left-14 bg-gradient-to-r from-blue-lighter' />
       <Button style='sliderButton' onClick={slideToLeft} 
-        className='absolute top-0 left-0 p-[14px] bg-white rounded-full'>
+        className='absolute top-0 z-50 left-0 p-3 2xl:p-[14px] bg-white rounded-full'>
         <ArrowLeftIcon className='w-5 h-5' />
       </Button>
-      <div ref={slider} className='flex mx-16 overflow-x-hidden gap-x-3'>
+      <div ref={slider} className='flex mx-12 overflow-x-hidden 2xl:mx-16 gap-x-3'>
         {tagsWithCategoryAll.map((tag, idx) => (
           <Link key={idx} href={idx > 1 ? `/blog?tag=${tag.toLowerCase()}` : '/blog'} 
             className={classNames((router.query.tag ? router.query.tag === tag.toLowerCase() : idx === 0)
@@ -39,9 +39,9 @@ export default function TabsFilter({tags}: TabsFilterProps) {
           </Link>
         ))}
       </div>
-      <div className='absolute top-0 z-50 w-10 h-full right-14 bg-gradient-to-l from-blue-lighter' />
+      <div className='absolute top-0 z-30 w-10 h-full right-12 2xl:right-14 bg-gradient-to-l from-blue-lighter' />
       <Button style='sliderButton' onClick={slideToRight} 
-        className='absolute right-0 top-0 p-[14px] bg-white rounded-full'>
+        className='absolute z-50 right-0 top-0 p-3 2xl:p-[14px] bg-white rounded-full'>
         <ArrowRightIcon className='w-5 h-5' />
       </Button>
     </div>
