def9ce6e6cd04fbe5a52130ab5bfd1f7c50e7a7b
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-05-02 18:27:46 +0100

    Update [id].tsx

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -4,6 +4,7 @@ import {promises as fs} from 'fs'
 import Layout from '@/components/organisms/layout'
 import PostDetail from '@/components/molecules/post'
 import path from 'path'
+
 type PostPageProps = {
   post: NotionPost
 } & PageProps
@@ -18,9 +19,11 @@ export default function Post({post, t}: PostPageProps) {
   )
 }
 
-export async function getServerSideProps(context) {
-  const postSlug = context.query.id
-  const post = await fs.readFile(path.join(process.cwd(), `/blog/posts/${postSlug}/post.json`), 'utf8')
+export async function getServerSideProps() {
+  // const postSlug = context.query.id
+  const post = {}
+  console.log(await fs.readdir(path.join(process.cwd())))
+  // const post = await fs.readFile(path.join(process.cwd(), `/blog/posts/${postSlug}/post.json`), 'utf8')
   return {
     props: {post}
   }
