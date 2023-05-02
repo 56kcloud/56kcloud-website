c5e769b3c8b3a22a506d4e317eae934210e329de
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-05-02 18:04:46 +0100

    wip

--- src/pages/api/post/[slug].ts
+++ src/pages/api/post/[slug].ts
@@ -1,6 +1,6 @@
 import {promises as fs} from 'fs'
 import path from 'path'
 export default async function handler(req, res) {
-  const file = await fs.readFile(path.join(process.cwd(), '/blog/posts', `${req.query.slug}.json`), 'utf8')
+  const file = await fs.readFile(path.join(process.cwd(), '/blog/posts', `${req.query.slug}`, 'post.json'), 'utf8')
   res.status(200).json(JSON.parse(file))
 }
