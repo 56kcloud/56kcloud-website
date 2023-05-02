4f0c1a137f2f730a997ad0862fe2111a28218039
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-05-02 20:32:48 +0100

    wip

---
+++ src/pages/api/post/[slug].tsx
@@ -0,0 +1,7 @@
+import {promises as fs} from 'fs'
+import path from 'path'
+
+export default async function handler(req, res) {
+  const file = await fs.readFile(path.join(process.cwd(), 'public/blog/posts', req.query.slug, 'post.json'), 'utf8')
+  res.status(200).json(JSON.parse(file))
+}
