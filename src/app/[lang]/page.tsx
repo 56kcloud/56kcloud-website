6656425e1d033129ed5d6efce2f5d1e5c04d15cd
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-14 12:08:50 +0200

    Update page.tsx

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,6 +1,6 @@
 // import {getPageComponents} from '@/utils/api/page'
 
-export default async function Home({params: {lang}}) {
+export default async function Home() {
   // return await getPageComponents(lang)
   return <div>TEST</div>
 }
