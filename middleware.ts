34ac5bb33e56541cd5c7e44291939932d261f4da
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-14 11:38:57 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import {NextResponse} from 'next/server'
-import i18n from './i18n'
 import type {NextRequest} from 'next/server'
 
 // /es/page-name -> rewrites to -> /es/page-name?lang=es
 export function middleware(request: NextRequest) {
-  const locale = request.nextUrl.locale || i18n.defaultLocale
+  const locale = request.nextUrl.locale
   request.nextUrl.searchParams.set('lang', locale)
   return NextResponse.rewrite(request.nextUrl)
 }
