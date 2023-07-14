93bd1505d03ea0a9d9303c7485306f5dbfa4d34d
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-14 11:53:06 +0200

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import {NextResponse} from 'next/server'
-import type {NextRequest} from 'next/server'
-
-// /es/page-name -> rewrites to -> /es/page-name?lang=es
-export function middleware(request: NextRequest) {
-  const locale = request.nextUrl.locale
-  request.nextUrl.searchParams.set('lang', locale)
-  return NextResponse.rewrite(request.nextUrl)
-}
