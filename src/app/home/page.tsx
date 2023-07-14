61fefa0d445c03343af807b38a3baf6e8cc571bf
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-14 13:42:25 +0200

    wip

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,10 @@
-// import {getPageComponents} from '@/utils/api/page'
+import {getPageComponents} from '@/utils/api/page'
 
 export default async function Home({params}: {params: {lang: string}}) {
-  // const components = await getPageComponents(params.lang)
+  const components = await getPageComponents(params.lang)
+  console.log(components)
   return <>
-    {params.lang}
+    {components}
+    {/* {params.lang} */}
   </>
 }
