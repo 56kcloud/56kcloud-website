3977c4f7f55f4b6b3c3dd73d741571e452833113
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-15 09:50:32 +0200

    wip

--- src/app/api/revalidate/[tag]/route.ts
+++ src/app/api/revalidate/[tag]/route.ts
@@ -1,10 +1,10 @@
 import {NextResponse} from 'next/server'
-// import {revalidateTag} from 'next/cache'
+import {revalidateTag} from 'next/cache'
 
-export async function POST() {
+export async function GET(request, {params}) {
   try {
-    // const tag = params.tag
-    // revalidateTag(tag)
+    const tag = params.tag
+    revalidateTag(tag)
     return NextResponse.json({revalidated: true, now: Date.now()})
   } catch (error) {
     return NextResponse.json({error: error.toString()})
