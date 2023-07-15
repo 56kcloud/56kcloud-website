b5219d15c23c0cef703b186b05e20130e0a9fb30
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-15 12:42:57 +0200

    wip

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,23 +1,15 @@
-// import {HeaderProps} from '@/components/organisms/header/header'
 import {ReactNode} from 'react'
-// import {getSingleTypeProps} from '@/utils/api/single-type'
-// import Navbar from '@/components/organisms/header/header'
 
-// export async function generateStaticParams() {
-//   return [{lang: 'en'}, {lang: 'fr'}]
-// }
+export async function generateStaticParams() {
+  return [{lang: 'en'}, {lang: 'fr'}]
+}
 
 export default async function Layout({children}: {children: ReactNode, params: {lang: string}}) {
 
   return (
     <div>
       <div className='bg-primary-50'>
-        <header>
-          {/* <Navbar {...headerProps}/> */}
-        </header>
-        <main>
-          {children}
-        </main>
+        {children}
       </div>
     </div>
   )
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,15 +1,5 @@
-// import {getPageComponents} from '@/utils/api/page'
-import {HeaderProps} from '@/components/organisms/header/header'
-import {getData} from '@/utils/api/page'
-import {getSingleTypeProps} from '@/utils/api/single-type'
+import {getPageComponents} from '@/utils/api/page'
 
-export default async function Home() {
-  const data = await getData()
-  const headerProps = (await getSingleTypeProps('header', 'en')) as HeaderProps //NEED TO IMPROVE THIS
-  console.log(headerProps)
-  // return await getPageComponents(params.lang)
-  return <div>
-    {data.now}
-    {JSON.stringify(headerProps.links)}
-  </div>
+export default async function Home({params}: {params: {lang: string}}) {
+  return await getPageComponents(params.lang)
 }
