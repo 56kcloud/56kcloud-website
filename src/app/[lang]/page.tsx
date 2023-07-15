7acd4c874e7fa81a525382f37d65672e11c4bf39
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-15 12:26:19 +0200

    wip

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,8 +1,12 @@
 // import {getPageComponents} from '@/utils/api/page'
+import {HeaderProps} from '@/components/organisms/header/header'
 import {getData} from '@/utils/api/page'
+import {getSingleTypeProps} from '@/utils/api/single-type'
 
 export default async function Home() {
   const data = await getData()
+  const headerProps = (await getSingleTypeProps('header', 'en')) as HeaderProps //NEED TO IMPROVE THIS
+  console.log(headerProps)
   // return await getPageComponents(params.lang)
   return <div>
     {data.now}
