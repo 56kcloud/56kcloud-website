e06ffb085c8737c3505c3abe4fc7c26cd2b27c02
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-15 10:48:19 +0200

    wip

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,6 +1,16 @@
 // import {getPageComponents} from '@/utils/api/page'
 
+async function getData() {
+  const res = await fetch('http://localhost:3000/api/date', {
+    next: {tags: ['date']}
+  })
+  return res.json()
+}
+
 export default async function Home() {
+  const data = await getData()
   // return await getPageComponents(params.lang)
-  return <div>HERE</div>
+  return <div>
+    {data.now}
+  </div>
 }
---
+++ src/app/api/date/route.ts
@@ -0,0 +1,13 @@
+import {NextResponse} from 'next/server'
+// import {revalidateTag} from 'next/cache'
+
+export async function GET() {
+  // try {
+  //   const tag = params.tag
+  //   revalidateTag(tag)
+  //   return NextResponse.json({revalidated: true, now: Date.now()})
+  // } catch (error) {
+  //   return NextResponse.json({error: error.toString()})
+  // }
+  return NextResponse.json({now: Date.now()})
+}
