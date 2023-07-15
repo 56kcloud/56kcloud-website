3fac99c7df89920c1aae6a26e3d15e326aed14b0
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-15 13:13:20 +0200

    Update single-type.ts

--- src/utils/api/single-type.ts
+++ src/utils/api/single-type.ts
@@ -1,17 +1,26 @@
-import {Fetcher} from '@/models/fetcher.model'
+// import {Fetcher} from '@/models/fetcher.model'
 import {components} from '@/utils/api/components'
 import {getPropsFromNestedObjects} from './page'
 import {strapiAPI, strapiAPIToken} from '../../../config'
+// import next from 'next/types'
 // import {strapiFetcher} from '../../../config'
 
 
 export async function getSingleTypeProps(component, lang: string) {
-  const strapiFetcher = new Fetcher(strapiAPI, {Authorization: `Bearer ${strapiAPIToken}`})
-  const res = await strapiFetcher.call(
-    {
-      path: `/api/${component}?populate=deep&locale=${lang}`,
-      method: 'GET'
-    }
-  )
+  // const strapiFetcher = new Fetcher(strapiAPI, {Authorization: `Bearer ${strapiAPIToken}`})
+  // const res = await strapiFetcher.call(
+  //   {
+  //     path: `/api/${component}?populate=deep&locale=${lang}`,
+  //     method: 'GET'
+  //   }
+  // )
+  const test = await fetch(`${strapiAPI}/api/${component}?populate=deep&locale=${lang}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${strapiAPIToken}`
+    },
+    next: {tags: ['strapi']}
+  })
+  const res = await test.json()
   return await getPropsFromNestedObjects(components[component].props, res.data.attributes)
 }
