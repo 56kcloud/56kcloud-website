f45e0bff448e5cde698512d6d5c44a8ed5ca7757
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-15 13:25:09 +0200

    wip

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,13 +1,21 @@
 // import {getPageComponents} from '@/utils/api/page'
-import {HeaderProps} from '@/components/organisms/header/header'
+// import {HeaderProps} from '@/components/organisms/header/header'
 import {getSingleTypeProps} from '@/utils/api/single-type'
+// import {usePathname} from 'next/navigation'
+import {headers} from 'next/headers'
+// import {useRouter} from 'next/navigation'
 
-export default async function Home({params}: {params: {lang: string}}) {
-  const header = await getSingleTypeProps('header', params.lang) as HeaderProps
+export default async function Home() {
+  // const path = usePathname()
+  // const router = useRouter()
+  const headersList = headers()
+  const host = headersList.get('host') || ''
+  console.log(host)
+  const header = await getSingleTypeProps(host)
   // return await getPageComponents(params.lang)
   return (
     <div>
-      {JSON.stringify(header.links)}
+      {JSON.stringify(header)}
     </div>
   )
 }
--- src/utils/api/single-type.ts
+++ src/utils/api/single-type.ts
@@ -1,12 +1,12 @@
 // import {Fetcher} from '@/models/fetcher.model'
-import {components} from '@/utils/api/components'
-import {getPropsFromNestedObjects} from './page'
-import {strapiAPI, strapiAPIToken} from '../../../config'
+// import {components} from '@/utils/api/components'
+// import {getPropsFromNestedObjects} from './page'
+// import {strapiAPI, strapiAPIToken} from '../../../config'
 // import next from 'next/types'
 // import {strapiFetcher} from '../../../config'
 
 
-export async function getSingleTypeProps(component, lang: string) {
+export async function getSingleTypeProps(path) {
   // const strapiFetcher = new Fetcher(strapiAPI, {Authorization: `Bearer ${strapiAPIToken}`})
   // const res = await strapiFetcher.call(
   //   {
@@ -14,13 +14,19 @@ export async function getSingleTypeProps(component, lang: string) {
   //     method: 'GET'
   //   }
   // )
-  const test = await fetch(`${strapiAPI}/api/${component}?populate=deep&locale=${lang}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${strapiAPIToken}`
-    },
+  // const test = await fetch(`${strapiAPI}/api/${component}?populate=deep&locale=${lang}`, {
+  //   method: 'GET',
+  //   headers: {
+  //     Authorization: `Bearer ${strapiAPIToken}`
+  //   },
+  //   next: {tags: ['strapi']}
+  // })
+  // const res = await test.json()
+  // return await getPropsFromNestedObjects(components[component].props, res.data.attributes)
+  // console.log(path)
+  const res  = await fetch(`https://${path}/api/date`, {
     next: {tags: ['strapi']}
   })
-  const res = await test.json()
-  return await getPropsFromNestedObjects(components[component].props, res.data.attributes)
+  return await res.json()
+  // return {}
 }
