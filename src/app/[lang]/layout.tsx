4905c8e1bd07d9ed8e6f39c995b02e2dfe67698a
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-15 10:48:19 +0200

    wip

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,19 +1,19 @@
 import {ReactNode} from 'react'
-import {getSingleTypeProps} from '@/utils/api/single-type'
-import Navbar, {HeaderProps} from '@/components/organisms/header/header'
+// import {getSingleTypeProps} from '@/utils/api/single-type'
+// import Navbar from '@/components/organisms/header/header'
 
 // export async function generateStaticParams() {
 //   return [{lang: 'en'}, {lang: 'fr'}]
 // }
 
-export default async function Layout({children, params}: {children: ReactNode, params: {lang: string}}) {
-  const headerProps = (await getSingleTypeProps('header', params.lang)) as HeaderProps //NEED TO IMPROVE THIS
+export default async function Layout({children}: {children: ReactNode, params: {lang: string}}) {
+  // const headerProps = (await getSingleTypeProps('header', params.lang)) as HeaderProps //NEED TO IMPROVE THIS
 
   return (
     <div>
       <div className='bg-primary-50'>
         <header>
-          <Navbar {...headerProps}/>
+          {/* <Navbar {...headerProps}/> */}
         </header>
         <main>
           {children}
