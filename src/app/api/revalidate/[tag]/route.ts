57061ac13cd8dae166e8883f6faf0884faa323ae
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-15 09:10:52 +0200

    [api/revalidate] Add dynamic page

--- src/app/api/revalidate/[tag]/route.ts
+++ src/app/api/revalidate/[tag]/route.ts
@@ -1,11 +1,9 @@
 import {NextResponse} from 'next/server'
 import {revalidateTag} from 'next/cache'
- 
-export const dynamic = 'force-dynamic'
 
-export async function GET(request) {
+export async function GET(request, {params}) {
   try {
-    const tag = request.nextUrl.searchParams.get('tag')
+    const tag = params.tag
     revalidateTag(tag)
     return NextResponse.json({revalidated: true, now: Date.now()})
   } catch (error) {
