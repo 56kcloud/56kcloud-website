07916278f272c1116254bd01a7ec4e45926e7afe
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-17 08:55:48 +0200

    Update route.ts

--- src/app/api/revalidate/[tag]/route.ts
+++ src/app/api/revalidate/[tag]/route.ts
@@ -5,7 +5,7 @@ export async function GET(request, {params}) {
   try {
     const tag = params.tag
     console.log('REVALIDATE TAG', tag)
-    revalidatePath('/en')
+    revalidatePath('/[lang]')
     return NextResponse.json({revalidated: true, now: Date.now()})
   } catch (error) {
     return NextResponse.json({error: error.toString()})
