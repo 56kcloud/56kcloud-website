8b0b15acfd6d1cc5583d519897eaef62af277315
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-17 13:39:10 +0200

    wip

--- config.ts
+++ config.ts
@@ -1,5 +1,6 @@
-// import {Fetcher} from '@/models/fetcher.model'
+import {Fetcher} from '@/models/fetcher.model'
 export const strapiAPI = process.env.STRAPI_API_URL || ''
 export const strapiAPIToken = process.env.STRAPI_API_TOKEN || ''
 export const hostname = 
   `http${process.env.NEXT_PUBLIC_ENV === 'local' ? '' : 's'}://${process.env.NEXT_PUBLIC_VERCEL_URL}` || ''
+export const strapiFetcher = new Fetcher(strapiAPI, {Authorization: `Bearer ${strapiAPIToken}`})
