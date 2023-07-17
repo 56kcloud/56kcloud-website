d9d6c5681e6de19bb55b659961d124a623433da8
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-17 08:03:30 +0200

    wip

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,7 +2,7 @@
 // import {HeaderProps} from '@/components/organisms/header/header'
 import {getSingleTypeProps} from '@/utils/api/single-type'
 // import {usePathname} from 'next/navigation'
-import {headers} from 'next/headers'
+// import {headers} from 'next/headers'
 // import {useRouter} from 'next/navigation'
 
 // export const dynamic = 'force-dynamic'
@@ -10,8 +10,8 @@ import {headers} from 'next/headers'
 export default async function Home() {
   // const path = usePathname()
   // const router = useRouter()
-  const headersList = headers()
-  const host = headersList.get('host') || ''
+  // const headersList = headers()
+  const host = 'localhost:3000'
   // console.log(host)
   const header = await getSingleTypeProps(host)
   // return await getPageComponents(params.lang)
