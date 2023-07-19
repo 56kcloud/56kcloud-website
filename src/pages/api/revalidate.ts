a671a90bb34cab9d52929732951ad0f8e21780db
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-07-19 15:04:54 +0200

    Update revalidate.ts

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -3,6 +3,8 @@ export default async function handler(req, res) {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
     await res.revalidate('/')
+    await res.revalidate('/de')
+    await res.revalidate('/fr')
     return res.json({revalidated: true})
   } catch (err) {
     // If there was an error, Next.js will continue
