c2f99680cf09e39a865a9bd5efc34b039f141056
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-09-15 08:15:23 +0100

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import {PageProps} from '@/models/page.mode'
 import {getPageProps} from '@/utils/cms/endpoints'
 import {pageRenderer} from '@/utils/cms/renderer/components'
 
-export default function SolutionsPage({components, openGraph, layout}: PageProps) {
+export default function HomePage({components, openGraph, layout}: PageProps) {
   return pageRenderer(components, openGraph, layout)
 }
 
