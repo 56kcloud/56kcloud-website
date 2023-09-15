b8d41474837b0735d9ff15e632681678238a54a8
Helder-SR <helder.santos@edu.vs.ch>
2023-09-15 16:38:46 +0200

    [homepage] Dispaly header and improve homepage

--- src/components/ui/organisms/heros/home/index.tsx
+++ src/components/ui/organisms/heros/home/index.tsx
@@ -20,14 +20,18 @@ export default function HomeHero({title, button, image}: HomeHeroProps) {
         blurDataURL={image.blurDataURL}
         width={image.width}
         height={image.height}
-        className='w-full pointer-events-none opacity-70'/>
+        className='w-full pointer-events-none'/>
       <div 
         className='absolute inset-0 flex flex-col items-center justify-center title'>
-        <h1 className='mx-10 mt-4 mb-8 text-4xl text-center text-black sm:mb-20 sm:mx-40'>
+        <h1
+          className={`mx-10 my-4 mt-20 text-xl text-center text-white sm:mx-30 sm:mt-10 sm:text-3xl md:text-5xl 
+          lg:text-6xl xl:text-7xl 2xl:text-8xl`}>
           {title}
         </h1>
         <Button
           tone='primary'
+          size='small'
+          className='p-2 mt-8 text-xs/3 lg:mt-20'
         >
           <Link href={button.href}>
             {button.title}
