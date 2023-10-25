3603c59530abc686669bede98c6b108730fb1f7f
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-10-25 17:50:22 +0100

    Update index.tsx

--- apps/www/src/components/ui/molecules/mardown/index.tsx
+++ apps/www/src/components/ui/molecules/mardown/index.tsx
@@ -16,6 +16,16 @@ export function MarkdownViewer({content, className}: {content: string, className
                   key={props['href']}
                   src={props['href']}
                   className='w-full rounded-lg h-[500px]'/>
+              } else if (element?.type === 'img') {
+                return (<div className='flex flex-col items-center justify-center'>
+                  {/*  eslint-disable-next-line @next/next/no-img-element  */} 
+                  <img
+                    alt=''
+                    src={element.props['src']}
+                    className='bg-white w-auto h-auto sm:h-full max-h-[418px] !mb-0'
+                  />
+                </div>
+                )
               } else {
                 return React.createElement(type, props, children)
               }
