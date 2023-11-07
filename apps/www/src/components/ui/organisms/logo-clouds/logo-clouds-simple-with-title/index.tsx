5a1caff4d76c3f41d0345bb36e1074885941c7f6
nicolasvarone <varonenico@gmail.com>
2023-11-07 21:01:35 +0100

    [ui] Update partners section

--- apps/www/src/components/ui/organisms/logo-clouds/logo-clouds-simple-with-title/index.tsx
+++ apps/www/src/components/ui/organisms/logo-clouds/logo-clouds-simple-with-title/index.tsx
@@ -8,24 +8,25 @@ export type LogoCloudsSimpleWithTitleProps = {
 
 export default function LogoCloudsSimpleWithTitle(props: LogoCloudsSimpleWithTitleProps) { 
   return (
-    <div className='py-24 bg-gray-900 sm:py-32'>
+    <div className='py-24 sm:py-32'>
       <div className='px-6 mx-auto max-w-7xl lg:px-8'>
         <h2 className='text-lg font-semibold leading-8 text-center text-white'>
           {props.title}
         </h2>
         <div
-          className='flex flex-col lg:flex-row flex-wrap justify-center items-center max-w-lg mx-auto mt-10 gap-x-8 \
-                     sm:gap-x-10 lg:mx-0 lg:max-w-none text-white gap-y-10 sm:max-w-xl '
-        >
+          className='grid grid-cols-1 lg:grid-cols-4 mt-7 gap-x-6 gap-y-8'>
           {props.companies.map((company, index) => (
-            <Image
-              key={index} 
-              className='object-contain w-[200px] max-h-12'
-              src={company.image.src}
-              alt={company.name}
-              width={company.image.width}
-              height={company.image.height}
-            />
+            <div
+              key={index}
+              className='flex items-center justify-center w-full h-36 bg-slate-800 rounded-xl'>
+              <Image
+                className='w-auto h-8'
+                src={company.image.src}
+                alt={company.name}
+                width={company.image.width}
+                height={company.image.height}
+              />
+            </div>
           ))}
         </div>
       </div>
