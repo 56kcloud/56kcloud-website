59131f6d57a4e5ed3c75d89d0b1fe1cf9aebf727
nicolasvarone <varonenico@gmail.com>
2023-11-08 09:41:09 +0100

    [ui] Update contact section

--- apps/www/src/components/ui/organisms/contact-sections/contact-split-with-pattern/index.tsx
+++ apps/www/src/components/ui/organisms/contact-sections/contact-split-with-pattern/index.tsx
@@ -90,84 +90,34 @@ export default function ContactSplitWithPattern(props: ContactSplitWithPatternPr
   }
   
   return (<div
-    className='relative bg-gray-900 isolate'
+    className='relative isolate'
     id='contact-section'
   >
-    <div className='grid grid-cols-1 mx-auto max-w-7xl lg:grid-cols-2'>
-      <div className='relative px-6 pt-24 pb-20 sm:pt-32 lg:static lg:px-8 lg:py-48'>
-        <div className='max-w-xl mx-auto lg:mx-0 lg:max-w-lg'>
-          <div className='absolute inset-y-0 left-0 w-full overflow-hidden -z-10 ring-1 ring-white/5 lg:w-1/2'>
-            <svg
-              className='absolute inset-0 h-full w-full stroke-gray-700 \
-                           [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]'
-              aria-hidden='true'
-            >
-              <defs>
-                <pattern
-                  id='54f88622-e7f8-4f1d-aaf9-c2f5e46dd1f2'
-                  width={200}
-                  height={200}
-                  x='100%'
-                  y={-1}
-                  patternUnits='userSpaceOnUse'
-                >
-                  <path
-                    d='M130 200V.5M.5 .5H200'
-                    fill='none'/>
-                </pattern>
-              </defs>
-              <svg
-                x='100%'
-                y={-1}
-                className='overflow-visible fill-gray-800/20'>
-                <path
-                  d='M-470.5 0h201v201h-201Z'
-                  strokeWidth={0}/>
-              </svg>
-              <rect
-                width='100%'
-                height='100%'
-                strokeWidth={0}
-                fill='url(#54f88622-e7f8-4f1d-aaf9-c2f5e46dd1f2)'/>
-            </svg>
-            <div
-              className='absolute -left-56 top-[calc(100%-13rem)] transform-gpu blur-3xl \
-                           lg:left-[max(-14rem,calc(100%-59rem))] lg:top-[calc(50%-7rem)]'
-              aria-hidden='true'
-            >
-              <div
-                className='aspect-[1155/678] w-[72.1875rem] bg-gradient-to-br from-[#80caff] to-[#4f46e5] opacity-20'
-                style={{
-                  clipPath:
-                      'polygon(74.1% 56.1%, 100% 38.6%, 97.5% 73.3%, 85.5% 100%, 80.7% 98.2%, 72.5% 67.7%, \
-                       60.2% 37.8%, 52.4% 32.2%, 47.5% 41.9%, 45.2% 65.8%, 27.5% 23.5%, 0.1% 35.4%, 17.9% 0.1%, 27.6% \
-                       23.5%, 76.1% 2.6%, 74.1% 56.1%)'
-                }}
-              />
-            </div>
-          </div>
-          <h2 className='text-3xl font-bold tracking-tight text-white'>{props.title}</h2>
-          <p className='mt-6 text-lg leading-8 text-gray-300'>
+    <div className='grid grid-cols-1 mx-auto max-w-7xl lg:grid-cols-2 bg-slate-800 rounded-xl'>
+      <div className='relative px-6 py-8 lg:static lg:pl-12 lg:pr-0 lg:py-14'>
+        <div className='!max-w-sm mx-auto lg:mx-0 lg:max-w-lg'>
+          <h2 className='text-3xl font-medium text-white sm:text-4xl'>{props.title}</h2>
+          <p className='mt-5 text-[20px] leading-8 text-slate-400 font-light'>
             {props.subtitle}
           </p>
-          <dl className='mt-10 space-y-4 text-base leading-7 text-gray-300'>
+          <dl className='mt-10 space-y-5 text-base leading-[26px] font-light text-slate-400'>
             {props.locations?.map((location) => (
               <a
                 key={location.address}
                 href={location.gMap}
                 target='_blank'
-                className='flex text-gray-400 gap-x-4'
+                className='flex gap-x-4'
               >
                 <dt className='flex-none'>
                   <span className='sr-only'>Address</span>
                   <BuildingOffice2Icon
-                    className='w-6 text-gray-400 h-7'
+                    className='w-6 h-6 translate-y-1 text-slate-400'
                     aria-hidden='true'/>
                 </dt>
                 <dd>
                   {location.address}
                   <br/>
-                  {location.city} {location.zipCode}, {location.country}
+                  {location.zipCode} {location.city}, {location.country}
                 </dd>
               </a>
             ))}
@@ -175,7 +125,7 @@ export default function ContactSplitWithPattern(props: ContactSplitWithPatternPr
         </div>
       </div>
       <div
-        className='px-6 pt-20 pb-24 sm:pb-32 lg:px-8 lg:py-48 lg:h-[840px]'
+        className='px-6 pt-20 pb-24 sm:pb-32 lg:pl-0 lg:pr-12 lg:py-14'
       >
         {isExploding && <ConfettiExplosion
           force={0.8}
@@ -191,7 +141,7 @@ export default function ContactSplitWithPattern(props: ContactSplitWithPatternPr
             </p>
           </div>
           : <form onSubmit={handleSubmit((data) => onSubmit(data as contactUsFormData))}>
-            <div className='max-w-xl mx-auto space-y-4 lg:mr-0 lg:max-w-lg'>
+            <div className='!max-w-full space-y-4'>
               {serverError ? (
                 <div
                   className='p-2 sm:p-3 text-sm text-red-600 bg-red-500/10 border border-red-500/50 rounded-lg \
@@ -217,7 +167,11 @@ export default function ContactSplitWithPattern(props: ContactSplitWithPatternPr
                 />
               </div>
               <div className='flex justify-end mt-8'>
-                <Button type='submit'>
+                <Button
+                  type='submit'
+                  shape='circle'
+                  size='large'
+                  className='px-5 text-md bg-sky-300 text-slate-900'>
                   Send Message
                 </Button>
               </div>
