ae5ba1f44f52189c5f1716c96ae2c73f0e090ae1
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-11-08 20:14:33 +0000

    [apps/www/components/blog-three-column] Fix height when having only one row

--- apps/www/src/components/ui/molecules/cards/article/index.tsx
+++ apps/www/src/components/ui/molecules/cards/article/index.tsx
@@ -33,7 +33,7 @@ export default function ArticleCard({article, sameHeight}: ArticleCardProps) {
           image={article.image}
           sameHeight={sameHeight}
         />
-        <div className='flex flex-col mt-9'>
+        <div className='flex flex-col flex-1 mt-9'>
           <div className='flex items-center gap-x-4'>
             <span className='text-sm font-light text-slate-400'>{publishedOn}</span>
             <ArticleTagList tags={article.tags}/>
@@ -44,23 +44,23 @@ export default function ArticleCard({article, sameHeight}: ArticleCardProps) {
               {article.title}
             </h1>
             <p 
-              className='mt-6 text-base font-light leading-[26px] text-slate-400 line-clamp-3'>
+              className='mt-6 text-base font-light leading-[26px] text-slate-400 line-clamp-3 h-20'>
               {article.description}
             </p>
-            <div
-              className='flex flex-wrap items-center mt-8 text-sm gap-x-3 text-slate-400'>
-              <Avatar
-                image={article.author.avatar.src}
-                alt={article.author.avatar.alt || 'author'}
-                size='md'
-              />
-              <div className='flex flex-col text-base'>
-                <span className='font-normal text-white'>
-                  {article.author.name}
-                </span>
-                <div className='flex font-light gap-x-2'>
-                  <span>{article.readTime} minute{article.readTime > 1 ? 's' : ''} read</span>
-                </div>
+          </div>
+          <div
+            className='flex flex-wrap items-center mt-8 text-sm gap-x-3 text-slate-400'>
+            <Avatar
+              image={article.author.avatar.src}
+              alt={article.author.avatar.alt || 'author'}
+              size='md'
+            />
+            <div className='flex flex-col text-base'>
+              <span className='font-normal text-white'>
+                {article.author.name}
+              </span>
+              <div className='flex font-light gap-x-2'>
+                <span>{article.readTime} minute{article.readTime > 1 ? 's' : ''} read</span>
               </div>
             </div>
           </div>
