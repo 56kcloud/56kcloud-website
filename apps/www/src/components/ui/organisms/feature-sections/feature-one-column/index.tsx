78d0d0277d4c5125b2637718d14edffca70661ed
nicolasvarone <varonenico@gmail.com>
2023-11-15 14:15:40 +0100

    wip

--- apps/www/src/components/ui/organisms/feature-sections/feature-one-column/index.tsx
+++ apps/www/src/components/ui/organisms/feature-sections/feature-one-column/index.tsx
@@ -1,6 +1,8 @@
+import {ArrowLongRightIcon} from '@heroicons/react/24/solid'
 import {Feature} from '@/models/feature.model'
-import {cn} from '@/utils/toolbox'
+import Button from '@/components/ui/atoms/button'
 import Icon from '@/components/ui/atoms/icon'
+import Link from 'next/link'
 
 export type FeatureOneColumnProps = {
   title: string
@@ -19,27 +21,32 @@ const FeatureOneColumn = (props: FeatureOneColumnProps) => {
               {props.subtitle}
             </p>
           </div>
-          <dl className='w-full mt-10 lg:mt-0 lg:pl-8 lg:w-3/6'>
+          <dl className='flex flex-col w-full mt-10 lg:mt-0 lg:w-3/6 gap-y-14'>
             {props.features?.map((feature, index) => (
-              <div
-                key={index}
-                className={cn(
-                  index === props.features.length - 1 ? 'last:border-b-0 pb-0' : 'pb-7', 
-                  index === 0 ? 'pt-0' : 'pt-7', 'border-b-[1px] border-slate-800')}>
-                <a href={feature.link}>
-                  <dt className='text-lg font-normal text-white'>
-                    <div className='flex items-center justify-center w-8 h-8 mb-6 rounded-lg'>
-                      <Icon
-                        {...feature.icon}
-                        className='w-auto h-8 fill-sky-300'
-                      />
-                    </div>
-                    {feature.title}
-                  </dt>
-                  <dd className='flex flex-col flex-auto mt-1 text-base font-light leading-[26px] text-slate-400'>
-                    <p className='flex-auto'>{feature.description}</p>
-                  </dd>
-                </a>
+              <div key={index}>
+                <dt className='flex flex-row items-center text-lg font-normal text-white gap-x-4'>
+                  <div className='flex items-center justify-center w-8 h-8 rounded-lg'>
+                    <Icon
+                      {...feature.icon}
+                      className='w-auto h-8 fill-sky-300'
+                    />
+                  </div>
+                  {feature.title}
+                </dt>
+                <dd className='flex flex-col flex-auto mt-1 text-base font-light leading-[26px] text-slate-400'>
+                  <p className='flex-auto'>{feature.description}</p>
+                </dd>
+                <Button
+                  asChild
+                  size='large'
+                  variant='link'
+                  className='mt-1 text-base text-sky-300 hover:text-violet-300'
+                  leading={<ArrowLongRightIcon className='w-7 h-7'/>}
+                >
+                  <Link href={feature.link}>
+                        Learn more
+                  </Link>
+                </Button>
               </div>
             ))}
           </dl>
