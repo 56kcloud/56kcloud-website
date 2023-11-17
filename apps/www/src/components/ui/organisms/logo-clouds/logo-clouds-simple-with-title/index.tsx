5d96525b6912b6ef1d5f17cf70564071270b1c6f
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-11-17 17:14:02 +0100

    [ui] Fix clouds logos responsive (#81)

--- apps/www/src/components/ui/organisms/logo-clouds/logo-clouds-simple-with-title/index.tsx
+++ apps/www/src/components/ui/organisms/logo-clouds/logo-clouds-simple-with-title/index.tsx
@@ -14,13 +14,14 @@ export default function LogoCloudsSimpleWithTitle(props: LogoCloudsSimpleWithTit
           {props.title}
         </h2>
         <div
-          className='grid grid-cols-2 lg:grid-cols-4 mt-7 gap-x-6 gap-y-8'>
+          className='grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-4 mt-7 gap-x-6 sm:gap-y-6'>
           {props.companies.map((company, index) => (
             <div
               key={index}
-              className='flex items-center justify-center w-full p-11 h-36 bg-slate-800 rounded-xl'>
+              className='flex items-center justify-center w-full p-7 sm:p-8 lg:p-11 sm:h-28 lg:h-36 bg-slate-800 \
+              rounded-xl'>
               <Image
-                className='max-h-11'
+                className='max-h-6 sm:max-h-8 lg:max-h-11'
                 src={company.image.src}
                 alt={company.name}
                 width={company.image.width}
