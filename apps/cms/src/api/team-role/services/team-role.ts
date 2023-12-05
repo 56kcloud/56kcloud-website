0211e5dbc1796d624182f7313adc87479acd19d6
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-12-05 20:22:49 +0000

    wip

--- apps/cms/src/api/team-role/services/team-role.ts
+++ apps/cms/src/api/team-role/services/team-role.ts
@@ -2,6 +2,6 @@
  * team-role service
  */
 
-import { factories } from '@strapi/strapi';
+import {factories} from '@strapi/strapi'
 
-export default factories.createCoreService('api::team-role.team-role');
+export default factories.createCoreService('api::team-role.team-role')
