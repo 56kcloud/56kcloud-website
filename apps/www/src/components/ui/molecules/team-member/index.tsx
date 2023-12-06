9c36b9d48aad5db918e5e1aea614f1acd7fbd44c
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-12-06 08:16:45 +0000

    Update index.tsx

--- apps/www/src/components/ui/molecules/team-member/index.tsx
+++ apps/www/src/components/ui/molecules/team-member/index.tsx
@@ -27,7 +27,7 @@ export default function TeamMemberCard({teamMember}: TeamMemberCardProps) {
               {teamMember.name}
             </h1>
             <p className='max-w-2xl mt-2 text-base text-white/90'>
-              {teamMember.bio}
+              {teamMember.role?.name}
             </p>
             <div className='flex space-x-4'>
               {teamMember.twitter &&
