0a59a5d64eb5fba793ca67375483439a4e470164
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-12-21 15:08:08 +0000

    [apps/www/api] Improve security with ACAs

--- apps/www/src/app/api/revalidate/route.ts
+++ apps/www/src/app/api/revalidate/route.ts
@@ -1,6 +1,31 @@
+import {NextResponse} from 'next/server'
 import {revalidateTag} from 'next/cache'
+
+export async function OPTIONS() {
+  return NextResponse.json(
+    {},
+    {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://cms.56k.cloud',
+        'Access-Control-Allow-Methods': 'OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      }
+    }
+  )
+}
 
 export async function GET() {
   revalidateTag('strapi')
-  return new Response('Strapi tag revalidated')
+  return new Response(
+    'Strapi tag revalidated',
+    {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://cms.56k.cloud',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      }
+    }
+  )
 }
