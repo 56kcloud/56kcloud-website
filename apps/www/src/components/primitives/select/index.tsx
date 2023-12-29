7a9363696ce7126c3cdf94062242bef37ee0110b
Nicolas Varone <varonenico@gmail.com>
2023-12-29 14:49:10 +0100

    wip

--- apps/www/src/components/primitives/select/index.tsx
+++ apps/www/src/components/primitives/select/index.tsx
@@ -18,7 +18,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      `flex h-10 w-full items-center justify-between rounded-md border bg-transparent px-3 py-2 text-sm 
+      `flex h-10 w-full items-center justify-between rounded-md border !bg-transparent px-3 py-2 text-sm 
        ring-offset-primary-50 placeholder:text-gray-300 focus:outline-none focus:ring-2 focus:ring-primary-500 
        focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`,
       className
--- apps/www/src/components/ui/organisms/hero-sections/hero-simple-center-with-background/index.tsx
+++ apps/www/src/components/ui/organisms/hero-sections/hero-simple-center-with-background/index.tsx
@@ -1,6 +1,7 @@
 import {CTAProps} from '@/models/cta.model'
 import {ImageProps} from '@/models/image.model'
 import Button from '@/components/ui/atoms/button'
+import Gradient from '@/../public/images/backgrounds/gradient.png'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -15,18 +16,12 @@ export type HeroSimpleCenterWithBackgroundProps = {
 export default function HeroSimpleCenterWithBackground(props: HeroSimpleCenterWithBackgroundProps) {
   return (
     <div className='relative overflow-hidden pt-28 isolate'>
-      <div
-        className='absolute inset-x-0 flex justify-center overflow-hidden top-4 -z-10 transform-gpu blur-3xl'
-        aria-hidden='true'
-      >
-        <div
-          className='aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25'
-          style={{
-            clipPath:
-                'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% \
-                 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% \
-                 51.7%)'
-          }}
+      <div className='absolute -right-20 -top-72'>
+        <Image
+          src={Gradient}
+          alt='Background'
+          width={1000}
+          height={1000}
         />
       </div>
       <div
@@ -71,7 +66,7 @@ export default function HeroSimpleCenterWithBackground(props: HeroSimpleCenterWi
           </div>
         </div>
         <div className='w-full lg:w-2/5'>
-          <div className='relative lg:w-full h-96'>
+          <div className='relative h-full lg:w-full'>
             <Image
               alt={'Background'}
               src={props.image.url}
