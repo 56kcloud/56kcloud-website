1eb2ea434971d3c17d1e9fb3d52f007d12a6ead8
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2024-01-05 22:23:58 +0000

    Update storybook-checker.ts

--- apps/www/scripts/storybook-checker.ts
+++ apps/www/scripts/storybook-checker.ts
@@ -3,7 +3,6 @@ import fg from 'fast-glob'
 import fs from 'fs'
 ;(async () => {
   const uiComponents = fg.sync(join(__dirname, `../src/components/ui/**/*/index.tsx`))
-  console.log(process.cwd())
   const componentsStories = await Promise.all(
     uiComponents.map(async (path) => {
       const splitedPath = path.split('/')
@@ -11,13 +10,7 @@ import fs from 'fs'
       const parent = splitedPath.join('/')
       const name = parent.split('/').pop()
       const fileExists = fs.existsSync(`${parent}/${name}.stories.tsx`)
-      if (fileExists) {
-        const projectName = process.cwd().split('/').pop() || ''
-        const relativePath = `.${parent.split(projectName)[1]}`
-        return {name, hasStory: '✅', path: `${relativePath}/${name}.stories.tsx`}
-      } else {
-        return {name, hasStory: '❌', path: ''}
-      }
+      return {name, hasStory: fileExists ? '✅' : '❌'}
     })
   )
   console.log(JSON.stringify(componentsStories))
