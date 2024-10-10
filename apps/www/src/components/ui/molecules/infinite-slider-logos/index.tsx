7b3af136110219379bd0a4f68878b4d154035a40
Nicolas Varone <varonenico@gmail.com>
2024-10-10 14:55:54 +0200

    wip

--- apps/www/src/components/ui/molecules/infinite-slider-logos/index.tsx
+++ apps/www/src/components/ui/molecules/infinite-slider-logos/index.tsx
@@ -1,4 +1,5 @@
 import {ImageProps} from '@/models/image.model'
+import {cn} from '@/utils/toolbox'
 import Image from 'next/image'
 
 export type InfiniteSliderLogosProps = {
@@ -9,6 +10,26 @@ export default function InfiniteSliderLogos(props: InfiniteSliderLogosProps) {
   const logosLength = props.logos.length
   const pixelsPerSecond = 100
   const animationSpeed = (500 * logosLength) / 2 / pixelsPerSecond
+  const shouldAnimate = logosLength > 3
+  const logosRow = (
+    <>
+      {props.logos.map((logo, index) => (
+        <div
+          key={props.logos.length + index}
+          className='flex items-center justify-center mx-4 w-[234px] p-7 sm:p-8 lg:p-10 bg-[#151E31] rounded-2xl \
+        border border-slate-800'
+        >
+          <Image
+            className='max-h-6 sm:max-h-8 lg:max-h-10'
+            src={logo.url}
+            alt={logo.alternateText || logo.name}
+            width={logo.width}
+            height={logo.height}
+          />
+        </div>
+      ))}
+    </>
+  )
 
   return (
     <div
@@ -16,45 +37,23 @@ export default function InfiniteSliderLogos(props: InfiniteSliderLogosProps) {
       [mask-image:_linear-gradient(to_right,_transparent_0,_white_96px,white_calc(100%-96px),_transparent_100%)]'
     >
       <div
-        className='flex'
+        className={cn(!shouldAnimate && '!w-full justify-center', 'flex')}
         style={{
           width: `calc(500px * ${logosLength})`,
-          animationName: `infiniteSliderAnimationFor${logosLength}`,
+          animationName: shouldAnimate ? `infiniteSliderAnimationFor${logosLength}` : 'none',
           animationTimingFunction: 'linear',
           animationIterationCount: 'infinite',
           animationDuration: `${animationSpeed}s`
         }}
       >
-        {props.logos.map((logo, index) => (
-          <div
-            key={props.logos.length + index}
-            className='flex items-center justify-center mx-4 w-[234px] p-7 sm:p-8 lg:p-10 bg-[#151E31] rounded-2xl \
-            border border-slate-800'
-          >
-            <Image
-              className='max-h-6 sm:max-h-8 lg:max-h-10'
-              src={logo.url}
-              alt={logo.alternateText || logo.name}
-              width={logo.width}
-              height={logo.height}
-            />
-          </div>
-        ))}
-        {props.logos.map((logo, index) => (
-          <div
-            key={props.logos.length + index}
-            className='flex items-center justify-center mx-4 w-[234px] p-7 sm:p-8 lg:p-10 bg-[#151E31] rounded-2xl \
-            border border-slate-800'
-          >
-            <Image
-              className='max-h-6 sm:max-h-8 lg:max-h-10'
-              src={logo.url}
-              alt={logo.alternateText || logo.name}
-              width={logo.width}
-              height={logo.height}
-            />
-          </div>
-        ))}
+        {shouldAnimate ? (
+          <>
+            {logosRow}
+            {logosRow}
+          </>
+        ) : (
+          <>{logosRow}</>
+        )}
       </div>
       <style>{`
         @keyframes infiniteSliderAnimationFor${logosLength} {
