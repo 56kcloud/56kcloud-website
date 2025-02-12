bd25de14b0a745cfbf649cf9afacff317978281b
Nicolas Varone <varonenico@gmail.com>
2025-02-12 17:53:37 +0700

    wip

---
+++ apps/cms/src/api/partner-page/controllers/partner-page.ts
@@ -0,0 +1,7 @@
+/**
+ * partner-page controller
+ */
+
+import {factories} from '@strapi/strapi'
+
+export default factories.createCoreController('api::partner-page.partner-page')
---
+++ apps/cms/src/api/partner-page/routes/partner-page.ts
@@ -0,0 +1,7 @@
+/**
+ * partner-page router
+ */
+
+import {factories} from '@strapi/strapi'
+
+export default factories.createCoreRouter('api::partner-page.partner-page')
