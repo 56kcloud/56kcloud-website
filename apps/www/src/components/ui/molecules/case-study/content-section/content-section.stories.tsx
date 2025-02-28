784bab507c62c426c95ace531d2f0dc28bb3ccf0
Nicolas Varone <varonenico@gmail.com>
2025-02-28 11:49:00 +0100

    [apps/www] Fix case study content section story

--- apps/www/src/components/ui/molecules/case-study/content-section/content-section.stories.tsx
+++ apps/www/src/components/ui/molecules/case-study/content-section/content-section.stories.tsx
@@ -1,19 +1,19 @@
 import {imageFactory} from '@/tests/factories/image.factory'
-import CaseStudy, {CaseStudyProps} from './index'
+import CaseStudyContentSection, {CaseStudyContentSectionProps} from './index'
 import type {Meta} from '@storybook/react'
 
 const meta = {
   title: 'components/Molecules/case-study',
-  component: CaseStudy,
+  component: CaseStudyContentSection,
   tags: ['autodocs'],
   args: {
     image: imageFactory()
   }
-} satisfies Meta<typeof CaseStudy>
+} satisfies Meta<typeof CaseStudyContentSection>
 
 export default meta
 
 export const Default = {
   name: 'Default',
-  render: (args: CaseStudyProps) => <CaseStudy {...args} />
+  render: (args: CaseStudyContentSectionProps) => <CaseStudyContentSection {...args} />
 }
