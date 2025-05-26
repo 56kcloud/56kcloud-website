3e8d14bc01c96dee58d98023271f56fb373ffbfa
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2025-05-26 17:45:52 +0100

    [apps/www] Improve banner responsiveness

--- apps/www/src/components/ui/molecules/banner/index.tsx
+++ apps/www/src/components/ui/molecules/banner/index.tsx
@@ -11,13 +11,13 @@ export type BannerProps = {
 export default function Banner(props: BannerProps) {
   return (
     <div className='w-full flex justify-center z-20'>
-      <div className='flex items-center bg-white/80 w-full gap-x-2 px-4 justify-center py-2'>
-        <p className='text-black text-sm line-clamp-1 flex-1 xl:flex-none'>{props.description}</p>
+      <div className='inline-flex items-center bg-white/80 w-full gap-x-2 justify-center py-2 px-4'>
+        <p className='text-black text-sm line-clamp-1 w-fit'>{props.description}</p>
         <Button
             asChild
             size='small'
             variant='link'
-            className='line-clamp-1'
+            className='line-clamp-1 !w-44'
             tone={props.cta.tone}
         >
             <a
