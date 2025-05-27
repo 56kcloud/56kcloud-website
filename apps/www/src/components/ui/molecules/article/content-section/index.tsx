16409fe18989aba8c4f94d6c15c8b11f65dee89a
Nicolas Varone <varonenico@gmail.com>
2025-05-27 15:20:52 +0200

    [apps/www] Fix design banner

--- apps/www/src/components/ui/molecules/article/content-section/index.tsx
+++ apps/www/src/components/ui/molecules/article/content-section/index.tsx
@@ -28,18 +28,18 @@ export default function ArticleContentSection({
 }: ArticleContentSectionProps) {
   content = `# ${title} \n ${content}`
   return (
-    <div className='flex items-center justify-center pt-32 sm:pt-52 pb-14'>
+    <div className='flex items-center justify-center pt-48 sm:pt-52 pb-14'>
       <div className='w-full px-6 mx-auto max-w-7xl lg:px-8'>
         <div className='z-50 w-full h-full overflow-auto bg-white/5 max-w-7xl rounded-xl overscroll-contain'>
           <ArticleCover image={image} />
-          <div className='w-full p-6 md:p-10'>
-            <div className='flex flex-col-reverse justify-between md:items-center md:flex-row'>
+          <div className='w-full p-6 pt-10 md:p-10'>
+            <div className='flex flex-col-reverse justify-between gap-y-4 md:items-center md:flex-row'>
               <Suspense>
                 <PostTagList tags={tags} />
               </Suspense>
               <div
-                className='flex flex-col items-center justify-center mb-10 overflow-hidden text-sm leading-6 \
-                 text-gray-300 md:flex-row md:mb-0 gap-y-6'
+                className='flex flex-row items-center justify-start overflow-hidden text-sm leading-6 \
+                 text-gray-300 gap-y-4'
               >
                 <time
                   dateTime={publishedOn}
@@ -47,10 +47,10 @@ export default function ArticleContentSection({
                 >
                   {formatDate(publishedOn)}
                 </time>
-                <div className='flex items-center -ml-4 gap-x-4'>
+                <div className='flex items-center gap-x-4 ml-4 md:-ml-4'>
                   <svg
                     viewBox='0 0 2 2'
-                    className='-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50 hidden md:flex'
+                    className='-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50 flex'
                   >
                     <circle
                       cx={1}
@@ -58,7 +58,10 @@ export default function ArticleContentSection({
                       r={1}
                     />
                   </svg>
-                  <Link href={`/about-us#${author.slug}`} className='flex gap-x-2.5 items-center hover:underline'>
+                  <Link
+                    href={`/about-us#${author.slug}`}
+                    className='flex gap-x-2.5 items-center hover:underline'
+                  >
                     <Avatar
                       size='sm'
                       image={author.avatar}
@@ -68,7 +71,7 @@ export default function ArticleContentSection({
                 </div>
               </div>
             </div>
-            <div className='max-w-full mt-10'>
+            <div className='max-w-full mt-16'>
               <MarkdownViewer
                 content={content}
                 className='px-0'
--- apps/www/src/components/ui/molecules/banner/index.tsx
+++ apps/www/src/components/ui/molecules/banner/index.tsx
@@ -1,4 +1,4 @@
-import { CTAProps } from '@/models/cta.model'
+import {CTAProps} from '@/models/cta.model'
 import {Dictionary} from '@/models/dictionary.model'
 import Button from '../../atoms/button'
 
@@ -10,25 +10,20 @@ export type BannerProps = {
 
 export default function Banner(props: BannerProps) {
   return (
-    <div className='w-full flex justify-center z-20 pt-4 px-4'>
-      <div className='inline-flex items-center w-full gap-x-4 justify-center py-2 px-4 max-w-7xl bg-primary-300/20 h-12 rounded-lg border border-primary-300/20 shadow-lg'>
+    <div className='w-full flex justify-center z-20 pt-2 px-4'>
+      <div className='inline-flex items-center w-full gap-x-4 justify-center py-2 px-4 max-w-[1232px] bg-primary-300/20 h-12 rounded-lg border border-primary-300/20 shadow-lg'>
         <span className='relative flex h-3 w-3'>
           <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-primary-300 opacity-75'></span>
           <span className='relative inline-flex rounded-full h-3 w-3 bg-primary-300'></span>
         </span>
         <p className='text-primary-100 text-sm line-clamp-1 flex-1 text-left'>{props.description}</p>
         <Button
-            asChild
-            size='small'
-            variant='link'
-            tone={props.cta.tone}
+          asChild
+          size='small'
+          variant='link'
+          tone={props.cta.tone}
         >
-            <a
-                href={props.cta.link}
-                target='_blank'
-            >
-              {props.cta.title}
-            </a>
+          <a href={props.cta.link}>{props.cta.title}</a>
         </Button>
       </div>
     </div>
