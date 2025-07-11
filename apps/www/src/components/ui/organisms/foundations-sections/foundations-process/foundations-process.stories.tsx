da06aa642b34a10fa5444bc63f9fcaf22bf7c45c
raphou00 <105916427+raphou00@users.noreply.github.com>
2025-07-11 14:56:03 +0200

    fix: fix foundations-process stories

--- apps/www/src/components/ui/organisms/foundations-sections/foundations-process/foundations-process.stories.tsx
+++ apps/www/src/components/ui/organisms/foundations-sections/foundations-process/foundations-process.stories.tsx
@@ -3,12 +3,11 @@ import {featureFactory} from '@/tests/factories/feature.factory'
 import FoundationsProcess, {type FoundationsProcessProps} from './index'
 import type {Meta} from '@storybook/react'
 
-const process = Array.from({length: 3}, (_, i) => ({
-  number: i + 1,
+const process = Array.from({length: 3}, () => ({
   title: faker.lorem.slug(),
   description: faker.lorem.sentence(),
   badge: faker.lorem.word(),
-  items: Array.from({length: 3}, () => faker.lorem.sentence(5))
+  items: Array.from({length: 3}, () => ({text: faker.lorem.sentence(5)}))
 }))
 
 const meta = {
@@ -18,7 +17,7 @@ const meta = {
   args: {
     title: faker.lorem.slug(),
     subtitle: faker.lorem.sentence(),
-    processes: process,
+    process: process,
     benefits: Array.from({length: 3}, () => featureFactory<'icon'>({iconType: 'outline'}))
   }
 } satisfies Meta<typeof FoundationsProcess>
