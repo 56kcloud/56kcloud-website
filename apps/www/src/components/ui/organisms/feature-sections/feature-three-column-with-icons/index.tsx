63593df959a8db4a0c361258fd817582d33b97a3
Nicolas Varone <varonenico@gmail.com>
2025-10-23 12:34:44 +0200

    wip

--- apps/www/src/components/ui/organisms/feature-sections/feature-three-column-with-icons/index.tsx
+++ apps/www/src/components/ui/organisms/feature-sections/feature-three-column-with-icons/index.tsx
@@ -27,15 +27,18 @@ export default function FeatureThreeColumnWithIcons(props: FeatureThreeColumnWit
             {props.features?.map((feature, index) => (
               <div
                 key={index}
-                className='relative w-full h-full flex flex-col items-start p-6 space-y-3 border border-slate-800 rounded-3xl bg-gradient-to-t from-slate-800 to-slate-900 sm:p-8'
+                className='relative w-full h-full flex flex-col pt-12 p-6 border border-slate-800 rounded-3xl bg-gradient-to-t from-slate-800 to-slate-900 sm:pt-12 sm:p-8'
               >
-                <div className='absolute top-0 left-1/2 -translate-x-1/2'>
-                  <Icon
-                    {...feature.icon}
-                    className='w-6 h-6 text-sky-500'
-                  />
+                <div className='absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                  <div className='w-14 h-14 rounded-full border-gradient bg-slate-800 flex items-center justify-center'>
+                    <Icon
+                      {...feature.icon}
+                      strokeWidth={1.25}
+                      className='w-7 h-7 text-slate-500'
+                    />
+                  </div>
                 </div>
-                <div className='space-y-3 text-center'>
+                <div className='space-y-4 text-center'>
                   <h3 className='text-2xl leading-7 font-semibold text-transparent bg-clip-text bg-text-gradient-blue'>
                     {feature.title}
                   </h3>
